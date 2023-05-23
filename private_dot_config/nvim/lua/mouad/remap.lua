e6aa95de400fcb6959671ad7abb8d1676e7a9435
Mouad Alami <moadalami40@gmail.com>
2023-05-23 12:48:45 +0100

    Update copy/past binds

--- private_dot_config/nvim/lua/mouad/remap.lua
+++ private_dot_config/nvim/lua/mouad/remap.lua
@@ -1,17 +1,13 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<space>j", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<space>k", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<space>q", "<cmd>cclose<CR>zz")
 
--- vim.keymap.set("n", "<space>n", "<cmd>next<CR>zz")
--- vim.keymap.set("n", "<space>p", "<cmd>cprev<CR>zz")
--- vim.keymap.set("n", "<space>j", "<cmd>lnext<CR>zz")
--- vim.keymap.set("n", "<space>k", "<cmd>lprev<CR>zz")
-
-vim.keymap.set('x', '<c-c>', '"+y')
 vim.keymap.set('n', 'cp', '"+p')
-
+vim.keymap.set('x', '<c-c>', '"+y')
+vim.keymap.set('x', '<c-x>', '"+d')
 vim.keymap.set('n', 'S', 'i<CR><Esc>')
 
 vim.keymap.set('n', '<F6>', ':setlocal spell! spelllang=fr<CR>')
