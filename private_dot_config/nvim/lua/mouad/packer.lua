6d87f07f1a6a6c01618821a85417f54f06e6178a
Mouad Alami <moadalami40@gmail.com>
2023-05-23 12:48:06 +0100

    Improved formatting

--- private_dot_config/nvim/lua/mouad/packer.lua
+++ private_dot_config/nvim/lua/mouad/packer.lua
@@ -4,64 +4,63 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-    use {
-	'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	-- or                            , branch = '0.1.x',
-	requires = { {'nvim-lua/plenary.nvim'} }
-    }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
 
-    use('ThePrimeagen/harpoon')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-    use {
-	'VonHeikemen/lsp-zero.nvim',
-	branch = 'v2.x',
-	requires = {
-	    -- LSP Support
-	    {'neovim/nvim-lspconfig'},             -- Required
-	    {                                      -- Optional
-	    'williamboman/mason.nvim',
-	    run = function()
-		pcall(vim.cmd, 'MasonUpdate')
-	    end,
-	},
-	{'williamboman/mason-lspconfig.nvim'}, -- Optional
+	use('ThePrimeagen/harpoon')
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' }, -- Required
+			{
+			                    -- Optional
+				'williamboman/mason.nvim',
+				run = function()
+					pcall(vim.cmd, 'MasonUpdate')
+				end,
+			},
+			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-	-- Autocompletion
-	{'hrsh7th/nvim-cmp'},     -- Required
-	{'hrsh7th/cmp-nvim-lsp'}, -- Required
-	{'L3MON4D3/LuaSnip'},     -- Required 
-    }
-}
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' }, -- Required
+			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
+			{ 'L3MON4D3/LuaSnip' }, -- Required
+		}
+	}
 
-use('tpope/vim-surround')
-use('anufrievroman/vim-angry-reviewer')
-use('tpope/vim-commentary')
-use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-}
+	use { 'tpope/vim-surround' }
+	use { 'anufrievroman/vim-angry-reviewer' }
+	use { 'tpope/vim-commentary' }
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+	}
 
-use('tommcdo/vim-lion')
-use('vimwiki/vimwiki')
-use("potamides/pantran.nvim") -- translation
-use {'nvim-orgmode/orgmode', config = function()
-  require('orgmode').setup{}
-end
-}
+	use { 'tommcdo/vim-lion' }
+	use { 'vimwiki/vimwiki'}
+	use { 'potamides/pantran.nvim' } -- translation
 
--- colorschemes
-use { "catppuccin/nvim", as = "catppuccin" }
-use { "rose-pine/neovim", as = "rose-pine" }
+	-- use { 'luk400/vim-jukit' } -- jupyter notebook alternative
 
-use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-	local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-	ts_update()
-    end,
-}
+	-- colorschemes
+	use { "catppuccin/nvim", as = "catppuccin" }
+	use { "rose-pine/neovim", as = "rose-pine" }
+
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,
+	}
 end)
