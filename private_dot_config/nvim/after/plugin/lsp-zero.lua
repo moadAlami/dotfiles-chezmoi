5db563544c7b3f75602ae1e8252281b30baba3a5
moadAlami <moadalami40@gmail.com>
2024-03-20 05:38:34 +0000

    Added keybindings for lsp-zero

--- private_dot_config/nvim/after/plugin/lsp-zero.lua
+++ private_dot_config/nvim/after/plugin/lsp-zero.lua
@@ -1,7 +1,14 @@
 local lsp_zero = require('lsp-zero')
-
 lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
+  local opts = {buffer = bufnr, remap = false}
+  -- lsp_zero.default_keymaps(opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  
 end)
 
 lsp_zero.set_sign_icons({
@@ -23,6 +30,7 @@ require('mason').setup({
 	}
     }
 })
+
 require('mason-lspconfig').setup({
   ensure_installed = {},
   handlers = {
@@ -35,4 +43,21 @@ require('mason-lspconfig').setup({
     end,
 
   },
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+-- this is the function that loads the extra snippets to luasnip
+-- from rafamadriz/friendly-snippets
+-- require('luasnip.loaders.from_vscode').lazy_load()
+
+cmp.setup({
+  formatting = lsp_zero.cmp_format({details = false}),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
 })
