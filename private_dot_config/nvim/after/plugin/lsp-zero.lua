3a84d9e94f27675292a93ded8d81ed3721e4000c
moadAlami <moadalami40@gmail.com>
2024-03-26 23:33:27 +0000

    Format with <leader>fo and hover with K

--- private_dot_config/nvim/after/plugin/lsp-zero.lua
+++ private_dot_config/nvim/after/plugin/lsp-zero.lua
@@ -2,13 +2,14 @@ local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
   -- lsp_zero.default_keymaps(opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
   vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  
+  vim.keymap.set("n", "<leader>fo", function() vim.lsp.buf.format() end, opts)
 end)
 
 lsp_zero.set_sign_icons({
