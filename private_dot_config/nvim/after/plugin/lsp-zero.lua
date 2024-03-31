0f73a9ed9b49a046b74d7a70e7db30c69993cc76
moadAlami <moadalami40@gmail.com>
2024-03-31 22:30:53 +0000

    Removed cmap config

--- private_dot_config/nvim/after/plugin/lsp-zero.lua
+++ private_dot_config/nvim/after/plugin/lsp-zero.lua
@@ -45,20 +45,3 @@ require('mason-lspconfig').setup({
 
   },
 })
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-
--- this is the function that loads the extra snippets to luasnip
--- from rafamadriz/friendly-snippets
--- require('luasnip.loaders.from_vscode').lazy_load()
-
-cmp.setup({
-  formatting = lsp_zero.cmp_format({details = false}),
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }),
-})
