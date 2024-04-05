68532c7f1f4bf6a3750ca23bb18e87201bb50114
Mouad Alami <mouad@alami.xyz>
2024-04-05 13:25:15 +0000

    Added keybindings

--- private_dot_config/nvim/after/plugin/lsp-zero.lua
+++ private_dot_config/nvim/after/plugin/lsp-zero.lua
@@ -45,3 +45,32 @@ require('mason-lspconfig').setup({
 
   },
 })
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+-- this is the function that loads the extra snippets to luasnip
+-- from rafamadriz/friendly-snippets
+require('luasnip.loaders.from_vscode').lazy_load()
+
+cmp.setup({
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'nvim_lua'},
+    {name = 'luasnip', keyword_length = 2},
+    {name = 'buffer', keyword_length = 3},
+  },
+  formatting = lsp_zero.cmp_format({details = false}),
+  mapping = cmp.mapping.preset.insert({
+    ['<S-tab>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<S-tab>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+})
