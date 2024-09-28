86901148ff0253dc5b1a534cbbf5c54b3b663a39
Mouad Alami <mouad@alami.xyz>
2024-09-28 11:17:15 +0100

    Added render-markdown.nvim plugin

--- private_dot_config/nvim/lua/mouad/packer.lua
+++ private_dot_config/nvim/lua/mouad/packer.lua
@@ -52,4 +52,15 @@ return require('packer').startup(function(use)
 	    {'L3MON4D3/LuaSnip'},
 	}
     }
+
+	use({
+		'MeanderingProgrammer/render-markdown.nvim',
+		after = { 'nvim-treesitter' },
+		requires = { 'echasnovski/mini.nvim', opt = true }, -- if you use the mini.nvim suite
+		-- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
+		-- requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+		config = function()
+			require('render-markdown').setup({})
+		end,
+	})
 end)
