03591c7d6ebe5bcf86c628153ea30d75b7877c4d
Mouad Alami <mouad@alami.xyz>
2025-07-21 16:50:53 +0100

    Updated debugger and added config for js (not working)

--- private_dot_config/nvim/after/plugin/dap.lua
+++ private_dot_config/nvim/after/plugin/dap.lua
@@ -20,17 +20,3 @@ dap.configurations.javascript = {
     cwd = "${workspaceFolder}",
   },
 }
-
-local dapui = require('dapui')
-dap.listeners.before.attach.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.launch.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
-end
--- private_dot_config/nvim/after/plugin/dapui.lua
+++ private_dot_config/nvim/after/plugin/dapui.lua
@@ -1,1 +1,6 @@
-require('dapui').setup()
+local dapui = require("dapui")
+
+dapui.setup()
+
+vim.keymap.set('n', '<leader>o', ':lua require("dapui").open()<CR>')
+vim.keymap.set('n', '<leader>O', ':lua require("dapui").close()<CR>')
