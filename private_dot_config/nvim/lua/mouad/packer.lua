b5e9d349b7d7f1ef678daf9ea695057ec7faec35
Mouad Alami <mouad@alami.xyz>
2025-07-21 10:37:43 +0100

    Disable latex use in render-markdown

--- private_dot_config/nvim/lua/mouad/packer.lua
+++ private_dot_config/nvim/lua/mouad/packer.lua
@@ -4,54 +4,53 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use { 'wbthomason/packer.nvim' }
-    use { 'nvim-lua/plenary.nvim' }
-    use { 'nvim-telescope/telescope.nvim' }
-    use { 'ThePrimeagen/harpoon' }
-    use { 'mbbill/undotree' }
-    use { 'tpope/vim-fugitive' }
-    use { 'tpope/vim-surround' }
-    use { 'anufrievroman/vim-angry-reviewer' }
-    use { 'tpope/vim-commentary' }
-    use {
-	'nvim-lualine/lualine.nvim',
-	requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
-
-    use { 'tommcdo/vim-lion' }
-    use { 'potamides/pantran.nvim' } -- translation
-
-    use { 'lewis6991/gitsigns.nvim' }
-
-    -- colorschemes
-    use { "catppuccin/nvim", as = "catppuccin" }
-    use { "rose-pine/neovim", as = "rose-pine" }
-
-    use {
-	'nvim-treesitter/nvim-treesitter',
-	run = function()
-	    local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-	    ts_update()
-	end,
-    }
-
-    use {
-	'VonHeikemen/lsp-zero.nvim',
-	branch = 'v3.x',
-	requires = {
-	    --- Uncomment the two plugins below if you want to manage the language servers from neovim
-	    {'williamboman/mason.nvim'},
-	    {'williamboman/mason-lspconfig.nvim'},
-
-	    -- LSP Support
-	    {'neovim/nvim-lspconfig'},
-	    -- Autocompletion
-	    {'hrsh7th/nvim-cmp'},
-	    {'hrsh7th/cmp-nvim-lsp'},
-	    {'L3MON4D3/LuaSnip'},
+	-- Packer can manage itself
+	use { 'wbthomason/packer.nvim' }
+	use { 'nvim-lua/plenary.nvim' }
+	use { 'nvim-telescope/telescope.nvim' }
+	use { 'ThePrimeagen/harpoon' }
+	use { 'mbbill/undotree' }
+	use { 'tpope/vim-fugitive' }
+	use { 'tpope/vim-surround' }
+	use { 'anufrievroman/vim-angry-reviewer' }
+	use { 'tpope/vim-commentary' }
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
 	}
-    }
+
+	use { 'tommcdo/vim-lion' }
+	use { 'potamides/pantran.nvim' } -- translation
+
+	use { 'lewis6991/gitsigns.nvim' }
+
+	-- colorschemes
+	use { "catppuccin/nvim", as = "catppuccin" }
+
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,
+	}
+
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			--- Uncomment the two plugins below if you want to manage the language servers from neovim
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	}
 
 	use({
 		'MeanderingProgrammer/render-markdown.nvim',
@@ -60,7 +59,15 @@ return require('packer').startup(function(use)
 		-- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
 		-- requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
 		config = function()
-			require('render-markdown').setup({})
+			require('render-markdown').setup({ latex = { enabled = false } })
 		end,
 	})
+
+	use {
+		'code-biscuits/nvim-biscuits',
+		requires = {
+			'nvim-treesitter/nvim-treesitter',
+			run = ':TSUpdate'
+		},
+	}
 end)
