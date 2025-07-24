0b3ac323af3ba5dfb2be8b33d94fcef778b95d85
Mouad Alami <mouad@alami.xyz>
2025-07-24 17:07:16 +0100

    hypr configs update

--- dot_local/bin/executable_scratch.sh
+++ dot_local/bin/executable_scratch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [ "$(tmux list-session | grep scratch)" ];then
-    st -t scratchpad -g 120x34 tmux attach -t scratch
+    st -c scratchpad -t scratchpad -g 120x34 tmux attach -t scratch
 else
-    st -t scratchpad -g 120x34 tmux new -s scratch
+    st -c scratchpad -t scratchpad -g 120x34 tmux new -s scratch
 fi
